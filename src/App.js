a4a1cfe754d134b4f35fc922fcf1349cfaa7e2ed
Shibnath16 <shibnathnandi1621@gmail.com>
2023-12-19 12:01:20 +0530

    rst

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { ThemeProvider } from "styled-components";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { darkTheme, lightTheme } from './utils/Themes.js'
 import Navbar from "./components/Navbar";
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HeroSection from "./components/HeroSection";
-import About from "./components/About";
+// import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -34,7 +34,7 @@ function App() {
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Router >
         <Navbar />
-        <Body>
+        <Body setDarkMode={setDarkMode}>
           <HeroSection />
           <Wrapper>
             <Skills />
